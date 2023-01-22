0e93f33f1a780b7278b17d71acdbbedc05248fcf
Enrique Santos <santosenrique869@gmail.com>
2023-01-22 13:58:51 -0300

    chore: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+
+export function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-violet-600">Hello world</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "index.html"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+    },
+    boxShadow: {
+      card: "0 0 1px 0 rgba(189,192, 207,0.06), 0 10px 16px -1px rgba(189,192, 207,0.2)",
+      cardHover:
+        "0 0 1px 0 rgba(189,192, 207,0.06), 0 10px 16px -1px rgba(189,192, 207,0.4)",
+    },
+  },
+  plugins: [],
+};
