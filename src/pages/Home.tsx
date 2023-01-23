aafc68b1cf469b7b2a950831896169c81f37a011
Enrique Santos <santosenrique869@gmail.com>
2023-01-23 15:06:24 -0300

    refactor: remove unused import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Loader, FormField, Card } from "~/components";
 import { useQuery } from "@tanstack/react-query";
 import { getAllPosts } from "~/services/get-all-post";
