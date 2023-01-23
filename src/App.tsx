c45d2bb51ecaadb333e43cf8994548fdd8104367
Enrique Santos <santosenrique869@gmail.com>
2023-01-23 14:36:44 -0300

    feat: create layout estruture in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import "@/styles/globals.css";
+import "~/styles/globals.css";
+import { Outlet, Link } from "react-router-dom";
+import logoImg from "~/assets/logo.svg";
 
 export function App() {
   return (
-    <div>
-      <h1 className="font-bold text-4xl text-violet-600">Hello world</h1>
-    </div>
+    <>
+      <header className="w-full bg-white flex items-center justify-between p-4 sm:px-8 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img
+            src={logoImg}
+            alt="logo open-ai"
+            className="w-28 object-contain"
+          />
+        </Link>
+
+        <Link
+          to="/create-post"
+          className="font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md transition-opacity hover:opacity-80"
+        >
+          Create
+        </Link>
+      </header>
+
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fefe] min-h-[calc(100vh-73px)]">
+        <Outlet />
+      </main>
+    </>
   );
 }
