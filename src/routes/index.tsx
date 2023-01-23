2d6ad387e64ff579cf6a9fac89f1d6b2f13655ee
Enrique Santos <santosenrique869@gmail.com>
2023-01-23 14:40:55 -0300

    feat: create all routes paths the app

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { App } from "~/App";
+import { Home, CreatePost } from "~/pages";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "create-post",
+          element: <CreatePost />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: "/app",
+  }
+);
