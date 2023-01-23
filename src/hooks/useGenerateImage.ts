0b6b704ee0f90d5e5068c12614a8454c564ef4a4
Enrique Santos <santosenrique869@gmail.com>
2023-01-23 16:00:54 -0300

    refactor: remove useless directive async in function handleCreateImage

--- src/hooks/useGenerateImage.ts
+++ src/hooks/useGenerateImage.ts
@@ -2,14 +2,14 @@ import { useMutation } from "@tanstack/react-query";
 import { generateImagePost } from "~/services";
 
 export function useGenerateImage() {
-  const { data, error, isLoading, mutateAsync } = useMutation({
+  const { data, error, isLoading, mutate } = useMutation({
     mutationFn: generateImagePost,
     onSuccess: (data) => (data.photo = `data:image/jpeg;base64,${data.photo}`),
   });
 
-  async function handleCreateImage(prompt: string) {
-    await mutateAsync({ prompt });
-  }
+  const handleCreateImage = (prompt: string) => {
+    mutate({ prompt });
+  };
 
   return { data, error, isLoading, handleCreateImage };
 }
