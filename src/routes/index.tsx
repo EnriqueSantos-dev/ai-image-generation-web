9b893a0edaa65633fb199756136bb7f24d05d9e2
Enrique Santos <santosenrique869@gmail.com>
2023-01-23 15:06:40 -0300

    refactor: remove basename app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,24 +2,19 @@ import { App } from "~/App";
 import { Home, CreatePost } from "~/pages";
 import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "create-post",
-          element: <CreatePost />,
-        },
-      ],
-    },
-  ],
+export const router = createBrowserRouter([
   {
-    basename: "/app",
-  }
-);
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "create-post",
+        element: <CreatePost />,
+      },
+    ],
+  },
+]);
